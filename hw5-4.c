339340b217b20c4d7778fbb29e5b953c196d9c7f
XC_night <146057378+BojungCHEN@users.noreply.github.com>
2023-11-06 13:31:36 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int H,M;
+    scanf("%d%d",&H,&M);
+    if(H<1 && H>12 && M<00 && M>60)
+        return 1;
+    float Dm1=6.00,Dh1=30.00,Dm,Dh,DF;
+    Dm=Dm1*M;
+    Dh=Dh1*H;
+    if(Dm>=Dh){
+    DF=Dm-Dh;
+    if(DF>180)
+        DF-=180;
+    }else{
+    DF=Dh-Dm;
+    if(DF>180)
+        DF-=180;
+    }
+    printf("%.3f %.3f %.3f",Dh,Dm,DF);
+
+    return 0;
+}
 
